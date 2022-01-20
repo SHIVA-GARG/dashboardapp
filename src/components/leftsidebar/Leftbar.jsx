9abc9e01f0cc846bbe0f09cacba2aa38df4cdf69
Shiva Garg <66199185+SHIVA-GARG@users.noreply.github.com>
2022-01-20 21:35:54 +0530

    commited

--- src/components/leftsidebar/Leftbar.jsx
+++ src/components/leftsidebar/Leftbar.jsx
@@ -1,3 +1,4 @@
+import { Dashboard,Clock,History, BarChart } from '@mui/icons-material';
 import './leftbar.css';
 
 const Leftbar = () => {
@@ -10,19 +11,19 @@ const Leftbar = () => {
                 </div>
 
                 <div className="reportPart">
-                    
+
                     <ul className="list-unstyled CTAs">
                         Reports
                         <li>
-                            <a href="#" className="download">Dashboard</a>
+                            <a href="#" className="download"><History/>Dashboard</a>
                         </li>
                         <li>
-                            <a href="#" className="download">Dashboard Alternative</a>
+                            <a href="#" className="download"><BarChart/>Dashboard Alternative</a>
                         </li>
 
                     </ul>
                 </div>
-                
+
                 <ul class="list-unstyled components">
                     Management
                     {/* <p className="reoprts">Reports</p> */}
@@ -47,36 +48,75 @@ const Leftbar = () => {
                                 <a href="#">Product 1</a>
                             </li>
                             <li>
-                                <a href="#">Product 1</a>
+                                <a href="#">Product 2</a>
                             </li>
                             <li>
-                                <a href="#">Edit Customer</a>
+                                <a href="#">Product 3</a>
                             </li>
                         </ul>
                     </li>
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Pages</a>
-                        <ul className="collapse list-unstyled" id="pageSubmenu">
+                    <li className="active">
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Orders</a>
+                        <ul className="collapse list-unstyled" id="homeSubmenu">
                             <li>
-                                <a href="#">Page 1</a>
+                                <a href="#">order 1</a>
                             </li>
                             <li>
-                                <a href="#">Page 2</a>
+                                <a href="#">order 2</a>
                             </li>
                             <li>
-                                <a href="#">Page 3</a>
+                                <a href="#">order 3</a>
                             </li>
                         </ul>
                     </li>
-                    <li>
-                        <a href="#">Portfolio</a>
+
+                    <li className="active">
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Invoices</a>
+                        <ul className="collapse list-unstyled" id="homeSubmenu">
+                            <li>
+                                <a href="#">invoice 1</a>
+                            </li>
+                            <li>
+                                <a href="#">invoice 2</a>
+                            </li>
+                            <li>
+                                <a href="#">invoice 3</a>
+                            </li>
+                        </ul>
                     </li>
-                    <li>
-                        <a href="#">Contact</a>
+                </ul>
+                <ul class="list-unstyled components">
+                    Applications
+                    {/* <p className="reoprts">Reports</p> */}
+                    <li className="active">
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Projects Platform</a>
+                        <ul className="collapse list-unstyled" id="homeSubmenu">
+                            <li>
+                                <a href="#">Project Platfrom 1</a>
+                            </li>
+                            <li>
+                                <a href="#">Project Platfrom 2</a>
+                            </li>
+                            <li>
+                                <a href="#">Project Platfrom 3</a>
+                            </li>
+                        </ul>
                     </li>
+                    <li className="active">
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Social Platform</a>
+                        <ul className="collapse list-unstyled" id="homeSubmenu">
+                            <li>
+                                <a href="#">Social Platfrom 1</a>
+                            </li>
+                            <li>
+                                <a href="#">Social Platfrom 2</a>
+                            </li>
+                            <li>
+                                <a href="#">Social Platfrom 3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    
                 </ul>
 
                 <ul className="list-unstyled CTAs">
