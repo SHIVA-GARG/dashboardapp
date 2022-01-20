e655e72cbb3699f52b994edf7cdb389e064eeaea
Shiva Garg <66199185+SHIVA-GARG@users.noreply.github.com>
2022-01-20 21:50:16 +0530

    leftbar corrected

--- src/components/leftsidebar/Leftbar.jsx
+++ src/components/leftsidebar/Leftbar.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import { Dashboard,Clock,History, BarChart, PersonAddAlt1, Redeem } from '@mui/icons-material';
-=======
-import { History, BarChart,PersonAddAlt1, Redeem } from '@mui/icons-material';
->>>>>>> faf66d0d9d80de822d1c23967a641be743edea4b
 import './leftbar.css';
 
 const Leftbar = () => {
     return (
-<<<<<<< HEAD
         
-=======
-        // <div className="uper"></div>
->>>>>>> faf66d0d9d80de822d1c23967a641be743edea4b
         <div className='left_sidebar-mine wrapper'>
             <nav id="sidebar">
                 <div class="sidebar-header">
@@ -24,17 +16,10 @@ const Leftbar = () => {
                     <ul className="list-unstyled CTAs">
                         Reports
                         <li>
-<<<<<<< HEAD
                             <a href="#" className="download"><History/>Dashboard</a>
                         </li>
                         <li>
                             <a href="#" className="download"><BarChart/>Dashboard Alternative</a>
-=======
-                            <a href="/" className="download"><History/>Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="/" className="download"><BarChart/>Dashboard Alternative</a>
->>>>>>> faf66d0d9d80de822d1c23967a641be743edea4b
                         </li>
 
                     </ul>
@@ -44,50 +29,34 @@ const Leftbar = () => {
                     Management
                     {/* <p className="reoprts">Reports</p> */}
                     <li className="active">
-<<<<<<< HEAD
                         <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle"><PersonAddAlt1/>Customers</a>
-=======
-                        <a href="/homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle"><PersonAddAlt1/>Customers</a>
->>>>>>> faf66d0d9d80de822d1c23967a641be743edea4b
                         <ul className="collapse list-unstyled" id="homeSubmenu">
                             <li>
-                                <a href="/">List Customers</a>
+                                <a href="#">List Customers</a>
                             </li>
                             <li>
-                                <a href="/">View Customer</a>
+                                <a href="#">View Customer</a>
                             </li>
                             <li>
-                                <a href="/">Edit Customer</a>
+                                <a href="#">Edit Customer</a>
                             </li>
                         </ul>
                     </li>
                     <li className="active">
-<<<<<<< HEAD
                         <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle"><Redeem/>Products</a>
-=======
-                        <a href="/homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle"><Redeem/>Products</a>
->>>>>>> faf66d0d9d80de822d1c23967a641be743edea4b
                         <ul className="collapse list-unstyled" id="homeSubmenu">
                             <li>
-                                <a href="/">Product 1</a>
+                                <a href="#">Product 1</a>
                             </li>
                             <li>
-<<<<<<< HEAD
                                 <a href="#">Product 2</a>
                             </li>
                             <li>
                                 <a href="#">Product 3</a>
-=======
-                                <a href="/">Product 2</a>
-                            </li>
-                            <li>
-                                <a href="/">Product 3</a>
->>>>>>> faf66d0d9d80de822d1c23967a641be743edea4b
                             </li>
                         </ul>
                     </li>
                     <li className="active">
-<<<<<<< HEAD
                         <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Orders</a>
                         <ul className="collapse list-unstyled" id="homeSubmenu">
                             <li>
@@ -98,24 +67,11 @@ const Leftbar = () => {
                             </li>
                             <li>
                                 <a href="#">order 3</a>
-=======
-                        <a href="/homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Orders</a>
-                        <ul className="collapse list-unstyled" id="homeSubmenu">
-                            <li>
-                                <a href="/">order 1</a>
-                            </li>
-                            <li>
-                                <a href="/">order 2</a>
-                            </li>
-                            <li>
-                                <a href="/">order 3</a>
->>>>>>> faf66d0d9d80de822d1c23967a641be743edea4b
                             </li>
                         </ul>
                     </li>
 
                     <li className="active">
-<<<<<<< HEAD
                         <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Invoices</a>
                         <ul className="collapse list-unstyled" id="homeSubmenu">
                             <li>
@@ -126,18 +82,6 @@ const Leftbar = () => {
                             </li>
                             <li>
                                 <a href="#">invoice 3</a>
-=======
-                        <a href="/homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Invoices</a>
-                        <ul className="collapse list-unstyled" id="homeSubmenu">
-                            <li>
-                                <a href="/">invoice 1</a>
-                            </li>
-                            <li>
-                                <a href="/">invoice 2</a>
-                            </li>
-                            <li>
-                                <a href="/">invoice 3</a>
->>>>>>> faf66d0d9d80de822d1c23967a641be743edea4b
                             </li>
                         </ul>
                     </li>
@@ -146,7 +90,6 @@ const Leftbar = () => {
                     Applications
                     {/* <p className="reoprts">Reports</p> */}
                     <li className="active">
-<<<<<<< HEAD
                         <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Projects Platform</a>
                         <ul className="collapse list-unstyled" id="homeSubmenu">
                             <li>
@@ -157,23 +100,10 @@ const Leftbar = () => {
                             </li>
                             <li>
                                 <a href="#">Project Platfrom 3</a>
-=======
-                        <a href="/homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Projects Platform</a>
-                        <ul className="collapse list-unstyled" id="homeSubmenu">
-                            <li>
-                                <a href="/">Project Platfrom 1</a>
-                            </li>
-                            <li>
-                                <a href="/">Project Platfrom 2</a>
-                            </li>
-                            <li>
-                                <a href="/">Project Platfrom 3</a>
->>>>>>> faf66d0d9d80de822d1c23967a641be743edea4b
                             </li>
                         </ul>
                     </li>
                     <li className="active">
-<<<<<<< HEAD
                         <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Social Platform</a>
                         <ul className="collapse list-unstyled" id="homeSubmenu">
                             <li>
@@ -184,18 +114,6 @@ const Leftbar = () => {
                             </li>
                             <li>
                                 <a href="#">Social Platfrom 3</a>
-=======
-                        <a href="/homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Social Platform</a>
-                        <ul className="collapse list-unstyled" id="homeSubmenu">
-                            <li>
-                                <a href="/">Social Platfrom 1</a>
-                            </li>
-                            <li>
-                                <a href="/">Social Platfrom 2</a>
-                            </li>
-                            <li>
-                                <a href="/">Social Platfrom 3</a>
->>>>>>> faf66d0d9d80de822d1c23967a641be743edea4b
                             </li>
                         </ul>
                     </li>
