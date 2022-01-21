7b39807a46ba7ea1a4b3411726cd0be2e60c3dd8
Shiva Garg <66199185+SHIVA-GARG@users.noreply.github.com>
2022-01-21 11:50:37 +0530

    upper icon changed

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,31 +1,51 @@
 import './topbar.css'
 // import {PhoneIcon} from "@material-ui/icons"
 // import {PhoneIcon} from '@mui/icons-material'
-import { Mail, ThreeDRotation } from '@mui/icons-material';
+import { Mail, ThreeDRotation, Search, Group } from '@mui/icons-material';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import { Person, PersonAdd, Settings, NotificationsNone,AccountCircle } from '@material-ui/icons'
 const Topbar = () => {
     return (
-        <div className='topbar' style={{height:"60px",marginBottom:"-30px"}}>
+        <div className='topbar' style={{ height: "60px", marginBottom: "-30px" }}>
 
             <div className="companylogo">
                 <div className="phoneContact">
-                   {/* <PhoneIcon/> */}
-                   <LocalPhoneIcon htmlColor='yellow'/>
+                    {/* <PhoneIcon/> */}
+                    <LocalPhoneIcon htmlColor='yellow' />
                     +91-9646565768
                 </div>
                 <div className="mailContact">
-                    <Mail htmlColor='yellow'/>
+                    <Mail htmlColor='yellow' />
                     aryanmehta@gmail.com
                 </div>
             </div>
-            <div className="rightbar">
+            <div className="rightbar d-flex" style={{justifyContent:"space-between"}}>
                 <div className="themes">
-                    Theme FAQ"s
+                    <Search style={{marginLeft:"10px"}}/>
                 </div>
                 <div className="needHelp">
-                    Need Help?
+                    <Group style={{marginLeft:"10px"}}/>
                 </div>
-                <div className="languageDropdown">
+                <div className="needHelp">
+                    <NotificationsNone  style={{marginLeft:"10px"}}/>
+                </div>
+                <div className="needHelp">
+                    <Settings  style={{marginLeft:"10px"}}/>
+                </div>
+
+                <div className='d-flex' style={{marginRight:"-10px"}}>
+                    <AccountCircle
+                        style={{
+                            fontSize: "30px",
+                            marginLeft:"10px",
+                            color: "#c0c0c0",
+                            // marginBlockEnd: "12px",
+                        }}
+                    />
+                    <p style={{ margin: "0",color:"white" ,margin:"2px 0 0 5px"}}>demo@devias.io</p>
+                    
+                </div>
+                {/* <div className="languageDropdown">
                     <div className="dropdown pull-right dropdown-left-manual">
                         <button className="dropdownItem btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                             en
@@ -50,7 +70,7 @@ const Topbar = () => {
                             <li><a className="dropdown-item" href="/">EUR</a></li>
                         </ul>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     );
