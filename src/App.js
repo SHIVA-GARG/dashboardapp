d8de7512c86c3394447f0bb2632c4f0b2347fd49
Suryansh <sgoyal2402@gmail.com>
2022-01-21 16:59:54 +0530

    few fixes for deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-
 import './App.css';
 import Topbar from './components/topbar/Topbar'
-import Navbar from './components/Navbar/Navbar'
 
 import Leftbar from './components/leftsidebar/Leftbar';
 import Rightpart from './components/RightPart/rightPart';
 import RightUpperPart from './components/RightPart/RightUpperPart';
-import styled from '@emotion/styled';
-
-const Box = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
 
 function App() {
   return (
     <div className="App">
       <Topbar />
-      {/* <Navbar/> */}
       <hr />
       <div className="fluid-container-xxl my-md-4 bd-layout">
       <Leftbar />
